26409a3907bb4ee9e7b8ef0f4fe6fce3aca67987
Louis Whitburn <whilo192@student.otago.ac.nz>
2021-04-04 22:38:57 +1200

    More Kalman filter changes

--- py-kalman/kalman.py
+++ py-kalman/kalman.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    DT = 0.1
+    DT = 0.001
     
     GN = 0.01
     
-    F = np.matrix([[1, DT, 0, 0, 0, 0], [0, 1, DT, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, DT, 0], [0, 0, 0, 0, 1, DT], [0, 0, 0, 0, 0, 1]])
+    #F = np.matrix([[1, DT, 0, 0, 0, 0], [0, 1, DT, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 1, DT, 0], [0, 0, 0, 0, 1, DT], [0, 0, 0, 0, 0, 1]])
+    F = np.matrix([[1, DT], [0, 1]])
 
-    x = np.matrix([[0]] * 6)
-    p = np.identity(6) * GN
+    x = np.matrix([[0]] * 2)
+    p = np.identity(2) * GN
     
-    q = np.identity(6) * GN
-    r = np.identity(6) * GN
+    q = np.identity(2) * GN
+    r = np.identity(2) * GN
 
-    H = np.matrix([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
+    #H = np.matrix([[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
 
-    for i in range(0,10):
-        w = np.random.normal(0, GN, (6, 1, 1))[0]
-        z = np.random.normal(0, GN, (1, 6, 1))[0]
+    B = np.matrix([[0.5 * DT ** 2], [DT]])
+
+    H = np.matrix([[0, 0], [0, 1]])
+
+    disp_x = []
+
+    t_vals = [x * DT for x in range(0,10000)]
+
+    v_accum = 0
+
+    for i in t_vals:
+        acc = -10 * np.cos(np.pi * i)
         
-        x_new = F.dot(x) # + B dot u
+        v_accum += acc * DT
+        
+        w = np.random.normal(0, GN, (2, 1))
+        z = np.random.normal(0, GN, (2, 1)) + [[0], [v_accum]]
+        
+        x_new = F.dot(x) + acc * B + w
         p_new = F.dot(p).dot(F.T)
         
         y = z - H.dot(x_new)
@@ -32,15 +49,17 @@ def main():
         
         x_new_new = x_new + k.dot(y)
               
-        p_new_new = (np.identity(6) - k.dot(H)).dot(p_new)
+        p_new_new = (np.identity(2) - k.dot(H)).dot(p_new)
         
         y_new = z - H.dot(x_new_new)
 
         x = x_new_new
         p = p_new_new
-        
-    print(x)
-    print(p)
+
+        disp_x += [float(x[0])]
+
+    plt.plot(t_vals, list(disp_x))
+    plt.show()
 
 if __name__ == "__main__":
     main()
