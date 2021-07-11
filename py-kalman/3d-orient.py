b1353689b2e64013c0dd59fbf6fad9bf3f987b57
Louis Whitburn <whilo192@student.otago.nz>
2021-07-12 01:17:51 +1200

    Now measures magnetic field

--- py-kalman/3d-orient.py
+++ py-kalman/3d-orient.py
@@ -14,14 +14,15 @@ F = np.matrix([[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0,
 t_vals = [x * DT for x in range(0,1000)]
 
 #u_k is gyro, B, and g
-B = np.matrix([[0, 0, 0, DT, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, DT, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, DT, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]])
+B = np.matrix([[0, 0, 0, DT, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, DT, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, DT, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]])
 
 
-time_range = list(np.arange(0.005, 0.1, 0.005))
+noise_range = list(np.arange(0.005, 0.1, 0.005))
+noise_range.reverse()
 
-rms = np.empty((len(time_range), 1))
+rms = np.empty((len(noise_range), 1))
 
-for (j, GN) in enumerate(time_range):
+for (j, GN) in enumerate(noise_range):
     p = np.identity(12) * GN
 
     kalman_state = np.empty((len(t_vals), 12))
@@ -30,36 +31,55 @@ for (j, GN) in enumerate(time_range):
     cov_state = np.empty((len(t_vals), 12))
 
     x = np.empty((12, 1))
+    x[0] = 0
+    x[1] = np.sin(2 * np.pi / 3)
+    x[2] = np.sin(4 * np.pi / 3)
 
     OMEGA = 10
 
     def gen_value(i, t):
-        actual = np.matrix([[np.sin(OMEGA * t)], [np.cos(OMEGA * t) - 1], [np.sin(OMEGA * t)], [OMEGA * np.cos(OMEGA * t)], [-OMEGA * np.sin(OMEGA * t)], [OMEGA * np.cos(OMEGA * t)], [0], [0], [0], [0], [0], [0]])
+        actual = np.matrix([[np.sin(OMEGA * t)], [np.sin(OMEGA * t + 2 * np.pi / 3)], [np.sin(OMEGA * t + 4 * np.pi / 3)], [OMEGA * np.cos(OMEGA * t)], [OMEGA * np.cos(OMEGA * t + 2 * np.pi / 3)], [OMEGA * np.cos(OMEGA * t + 4 * np.pi / 3)], [np.sin(OMEGA * t)], [np.sin(OMEGA * t + 2 * np.pi / 3)], [np.sin(OMEGA * t + 4 * np.pi / 3)], [0], [0], [0]])
         
         act_state[i] = actual.T
         
         return actual
 
+    i_exit = 0
+
     for (i, t) in enumerate(t_vals):
         actual = gen_value(i, t)
         
         H = np.zeros((12, 12))
-        H[0,3] = DT
-        H[1,4] = DT
-        H[2,5] = DT
-        
-        u = actual + np.random.normal(0, GN, (12, 1))
+        H[3, 3] = 1
+        H[4, 4] = 1
+        H[5, 5] = 1
+        H[0, 6] = 1
+        H[1, 7] = 1
+        H[2, 8] = 1
+        H[6, 6] = 1
+        H[7, 7] = 1
+        H[8, 8] = 1
+               
+        u = np.copy(actual)
         u[0] = 0
         u[1] = 0
         u[2] = 0
+
+
+        #Calculate our measurements
+        z = H @ actual + np.random.normal(0, GN, (12, 1))
+        z[9] = 0
+        z[10] = 0
+        z[11] = 0
         
-        z = H @ actual
-        
-        r = np.random.normal(0, GN, (12, 12))
-        q = np.random.normal(0, GN, (12, 1))
-        
+        r = np.identity(12) * GN
+        q = np.identity(12) * GN
         
         x_new = F @ x + B @ u + np.random.normal(0, GN, (12, 1))
+        x_new[9] = 0
+        x_new[10] = 0
+        x_new[11] = 0
+        
         p_new = F @ p @ F.T + q
         
         y = z - H @ x_new
@@ -118,7 +138,7 @@ axes[3].set(xlabel = "Time (s)", ylabel = "Covariance")
 plt.show()
 
 (fig, axes) = plt.subplots(1, sharex=True, figsize=(15, 10))
-axes.plot(time_range, rms, label=r"RMS of Error")
+axes.plot(noise_range, rms, label=r"RMS of Error")
 axes.grid(True)
 axes.set_title("RMS of Error over Estimated Measurement and State Transition Noise")
 axes.set(ylabel = "RMS of Error")
